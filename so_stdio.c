ee4689168f91992160c2f8c04ccb236515e6aa7c
Burcea Ionut <93282350+BurceaIonut@users.noreply.github.com>
2023-11-18 02:35:50 +0200

    Create so_stdio.c

---
+++ so_stdio.c
@@ -0,0 +1,275 @@
+#include "so_stdio.h"
+#include <stdbool.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+#define BUFFER_SIZE 4
+
+typedef struct _so_file
+{
+    int fd;
+    long cursor;
+    long index_buffer;
+    int mode;
+    char buffer[BUFFER_SIZE];
+    int isEOF;
+    int error;
+    int read_bytes;
+    int last_operation;
+    int fflush;
+    int child_pid;
+    int from_fread;
+}SO_FILE;
+
+SO_FILE *so_fopen(const char *pathname, const char *mode)
+{
+    SO_FILE* handle = (SO_FILE*)malloc(sizeof(SO_FILE));
+    if(handle == NULL)
+        return NULL;
+    memset(handle->buffer, 0, sizeof(handle->buffer));
+    handle->cursor = 0;
+    handle->error = 0;
+    handle->isEOF = 0;
+    handle->read_bytes = 0;
+    handle->mode = 0;
+    handle->child_pid = -1;
+    handle->last_operation = 0;
+    handle->index_buffer = 0;
+    handle->fflush = 0;
+    handle->from_fread = 0;
+    int fd;
+    if(strcmp(mode, "r") == 0)
+    {
+        handle->mode = 1;
+        handle->cursor = 0;
+        fd = open(pathname, O_RDONLY);
+    }
+    else if(strcmp(mode, "r+") == 0)
+    {
+        handle->mode = 2;
+        handle->cursor = 0;
+        fd = open(pathname, O_RDWR);
+    }
+    else if(strcmp(mode, "w") == 0)
+    {
+        handle->mode = 3;
+        handle->cursor = 0;
+        fd = open(pathname, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    }
+    else if(strcmp(mode, "w+") == 0)
+    {
+        handle->mode = 4;
+        handle->cursor = 0;
+        fd = open(pathname, O_RDWR | O_CREAT | O_TRUNC, 0600);
+    }
+    else if(strcmp(mode, "a") == 0)
+    {
+        handle->mode = 5;
+        fd = open(pathname, O_WRONLY | O_APPEND | O_CREAT, 0600);
+        handle->cursor = lseek(fd, 0, SEEK_CUR);//aici nu imi intoarce pozitia buna
+    }
+    else if(strcmp(mode, "a+") == 0)
+    {
+        handle->mode = 6;
+        fd = open(pathname, O_RDWR | O_APPEND | O_CREAT, 0600);
+        handle->cursor = 0;
+    }
+    if(handle->mode == 0)
+    {
+        handle->error = 1;
+        free(handle);
+        handle = NULL;
+        return NULL;
+    }
+    if(fd < 0)
+    {
+        handle->error = 1;
+        free(handle);
+        handle = NULL;
+        return NULL;
+    }
+    handle->fd = fd;
+    return handle;
+}
+
+int so_fclose(SO_FILE *stream)
+{
+    if(stream->last_operation == 1)
+    {
+        stream->read_bytes = so_fflush(stream);
+        if(stream->read_bytes < 0)
+        {
+            stream->error = 1;
+            return SO_EOF;
+        }
+    }
+    if(stream == NULL)
+        return SO_EOF;
+    if(close(stream->fd) < 0)
+        return SO_EOF;
+    free(stream);
+    stream = NULL;
+    return 0;
+}
+
+int so_fgetc(SO_FILE *stream)
+{
+    if(stream->last_operation == 1 && stream->fflush == 0)
+    {
+        stream->error = 1;
+        return SO_EOF;
+    }
+    if(stream->mode == 1 || stream->mode == 2 || stream->mode == 4 || stream->mode == 6)
+    {
+        if(stream->index_buffer == 0 || stream->index_buffer == BUFFER_SIZE || stream->last_operation == 1)
+        {
+            stream->read_bytes = read(stream->fd, stream->buffer, BUFFER_SIZE);
+            if(stream->read_bytes < 0)
+            {
+                stream->error = 1;
+                return SO_EOF;
+            }
+            else if(stream->read_bytes == 0)
+            {
+                stream->isEOF = 1;
+                stream->error = 1;
+                return SO_EOF;
+            }
+            else if(stream->read_bytes > 0)
+            {
+                stream->last_operation = 2;
+                stream->index_buffer = 0;
+            }
+        }
+        if(stream->from_fread == 0 && stream->buffer[stream->index_buffer] == '\0')
+        {
+            stream->isEOF = 1;
+            return SO_EOF;
+        }
+        stream->last_operation = 2;
+        stream->index_buffer++;
+        stream->cursor++;
+        stream->fflush = 0;
+        return (unsigned char)stream->buffer[stream->index_buffer - 1];
+    }
+    else
+    {
+        stream->last_operation = 2;
+        stream->error = 1;
+        return SO_EOF;
+    }
+}
+
+int so_fputc(int c, SO_FILE *stream)
+{
+    if(stream->last_operation == 2 && stream->fflush == 0)
+    {
+        stream->error = 1;
+        return SO_EOF;
+    }
+    if(stream->mode == 2 || stream->mode == 3 || stream->mode == 4 || stream->mode == 5 ||stream->mode == 6)
+    {
+        if(stream->index_buffer == BUFFER_SIZE)
+        {
+            stream->fflush = 0;
+            stream->read_bytes = so_fflush(stream);
+            if(stream->read_bytes < 0)
+            {
+                stream->error = 1;
+                return SO_EOF;
+            }
+        }
+        stream->buffer[stream->index_buffer] = (unsigned char)c;
+        stream->index_buffer++;
+        stream->cursor++;
+        stream->last_operation = 1;
+        stream->fflush = 0;
+        return (unsigned char)c;
+    }
+    else
+    {
+        stream->error = 1;
+        stream->last_operation = 1;
+        return SO_EOF;
+    }
+}
+
+int so_fflush(SO_FILE* stream)
+{
+    if(stream->last_operation == 1 && stream->index_buffer != 0)
+    {
+        stream->read_bytes = write(stream->fd, stream->buffer, stream->index_buffer);
+        memset(stream->buffer, 0, BUFFER_SIZE);
+        if(stream->read_bytes < 0)
+        {
+            stream->error = 1;
+            return SO_EOF;
+        }
+    }
+    else if(stream->last_operation == 2 && stream->index_buffer != 0)
+    {
+        memset(stream->buffer, 0, BUFFER_SIZE);
+        stream->index_buffer = 0;
+    }
+    stream->fflush = 1;
+    return 0;
+}
+
+int so_fseek(SO_FILE *stream, long offset, int whence)
+{
+    stream->read_bytes = so_fflush(stream);
+    if(stream->read_bytes < 0)
+    {
+        stream->error = 1;
+        return SO_EOF;
+    }
+    if(stream == NULL)
+    {
+        stream->error = 1;
+        return SO_EOF;
+    }
+    int pos = lseek(stream->fd, 0, SEEK_CUR);
+    pos =lseek(stream->fd, -(pos-stream->cursor), SEEK_CUR);
+    pos = lseek(stream->fd, offset, whence);
+    if(pos < 0)
+        return SO_EOF;
+    stream->cursor = pos;
+    return 0;
+}
+
+long so_ftell(SO_FILE *stream)
+{
+    if(stream == NULL)
+        return SO_EOF;
+    if(stream->error == 1)
+        return SO_EOF;
+    return stream->cursor;
+}
+
+int so_fileno(SO_FILE *stream)
+{
+    if(stream == NULL)
+        return SO_EOF;
+    return stream->fd;
+}
+
+int so_feof(SO_FILE *stream)
+{
+    if(stream == NULL)
+        return SO_EOF;
+    return stream->isEOF;
+}
+
+int so_ferror(SO_FILE *stream)
+{
+    if(stream == NULL)
+        return SO_EOF;
+    if(stream->error)
+        return 1;
+    return 0;
+}
